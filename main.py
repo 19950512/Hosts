7c68c78ef7533a7b182c5a95c719b5ff46492d60
19950512 <36517599+19950512@users.noreply.github.com>
2020-02-11 13:00:08 -0300

    Atualização Menu Host && Apache2
    -

--- main.py
+++ main.py
@@ -3,13 +3,18 @@
 
 import sys, os
 
+from os.path import join, dirname
+
+import dotenv
+dir(dotenv)
+
 sys.path.append(os.path.abspath('./Modules/'))
 
 import Host, Apache2, DevNux
 
-Host = Host()
+Host 	= Host()
 Apache2 = Apache2()
-DevNux = DevNux()
+DevNux 	= DevNux()
 
 while True:
 
@@ -30,6 +35,7 @@ while True:
 		while True:
 
 			DevNux.clear()
+			
 			print('########### VIRTUAL-HOSTS > HOSTS ###########')
 			print('\n')
 			print('1) Adicionar um novo Host')
@@ -95,31 +101,184 @@ while True:
 				break
 
 
+	if(acao == '2'):
+		
+		while True:
+
+			DevNux.clear()
+			
+			print('########### VIRTUAL-HOSTS > APACHE2 ###########')
+			print('\n')
+			print('1) Adicionar um novo Host-Virtual')
+			print('2) Ver Carrinho')
+			print('3) Salvar tudo')
+			print('4) Listar todos do arquivo .hosts')
+			print('0) Voltar')
+			
+			acaoApache = input('')
+			domain = '';
+
+			DevNux.clear()
+			
+			if acaoApache == '1':
+				
+				print('Informe o domínio')
+				domain = input('')
+				
+				print('Informe o diretório do projeto')
+				diretorio = input('')
+
+				res = Apache2.add(domain, diretorio)
+				print(res)
+				input('Pressione Enter para voltar')
+
+
+			# Retorna a Lista de Virtual-Hosts, oque estão na lista para serem salvos
+			if acaoApache == '2':
+
+				res = Apache2.getHosts()
+
+				if(len(res) == 0):
+					print('Nenhum virtual-host adicionado no carrinho.')
+				else:
+					print(res)
+
+				input('Pressione Enter para voltar.')
+
+			# Salva os Virtuais-hosts que estão na Lista
+			if acaoApache == '3':
+
+				res = Apache2.save()
+				print(res)
+				input('Pressione Enter para voltar.')
+
+			# Exibe todos os hosts que estão no .hosts
+			if acaoApache == '4':
+
+				res = Apache2.getAll()
+
+				if(len(res) == 0):
+					print('Nenhum virtual-host configurado no diretório do apache2.')
+				else:
+					for i in range(0, len(res)):
+						print(res[i])
+
+				input('Pressione Enter para voltar.')
+			
+
+			# Se for Voltar ao Menu
+			if(acaoApache == '0'):
+				break
+
+
+	if(acao == '3'):
+		
+		while True:
+
+			DevNux.clear()
+			
+			print('########### VIRTUAL-HOSTS > HOST && APACHE2 ###########')
+			print('\n')
+			print('1) Adicionar um novo Site')
+			print('2) Ver Carrinho')
+			print('3) Salvar tudo')
+			print('4) Listar todos os projetos')
+			print('0) Voltar')
+			
+			acaoApacheHost = input('')
+			domain = '';
+
+			DevNux.clear()
+			
+			if acaoApacheHost == '1':
+				
+				print('Informe o domínio')
+				domain = input('')
+				
+				print('Informe o diretório do projeto')
+				diretorio = input('')
+
+				# Host-Virtual
+				res = Apache2.add(domain, diretorio)
+				print(res)
+				input('Pressione Enter para voltar')
+
+				# Host
+				ip = '127.0.0.1'
+				res = Host.add(domain, ip)
+				print(res)
+				input('Pressione Enter para voltar')
+
+
+			# Retorna a Lista de Virtual-Hosts, oque estão na lista para serem salvos
+			if acaoApacheHost == '2':
+
+				# Apache2
+				res = Apache2.getHosts()
+
+				if(len(res) == 0):
+					print('Nenhum virtual-host adicionado no carrinho.')
+				else:
+					print(res)
+
+				input('Pressione Enter para voltar.')
+
+				# Hosts
+				res = Host.getDominios()
+
+				if(len(res) == 0):
+					print('Nenhum host adicionado no carrinho.')
+				else:
+					print(res)
+
+				input('Pressione Enter para voltar.')
+
+
+			# Salva os Virtuais-hosts que estão na Lista
+			if acaoApacheHost == '3':
+
+				# Apache2
+				res = Apache2.save()
+				print(res)
+				input('Pressione Enter para voltar.')
+
+				# Hosts
+				res = Host.save()
+				print(res)
+				input('Pressione Enter para voltar.')
+
+			# Exibe todos os hosts que estão no .hosts
+			if acaoApacheHost == '4':
+
+				# Apache2
+				res = Apache2.getAll()
+
+				if(len(res) == 0):
+					print('Nenhum virtual-host configurado no diretório do apache2.')
+				else:
+					for i in range(0, len(res)):
+						print(res[i])
+
+				input('Pressione Enter para voltar.')
+			
+
+				# Host
+				res = Host.getAll()
+
+				if(len(res) == 0):
+					print('Nenhum host configurado no arquivo ".hosts".')
+				else:
+					for i in range(0, len(res)):
+						print(res[i])
+
+				input('Pressione Enter para voltar.')
+
+
+			# Se for Voltar ao Menu
+			if(acaoApacheHost == '0'):
+				break
+
 	# Se for Sair
 	if(acao == '0'):
 		print('CYA.. :*')
 		break
-
-	# # Adicionando um host
-	# if acao == '1':
-
-	# 	print('Informe o domínio')
-	# 	domain = input('')
-		
-	# 	print('Informe o IP')
-	# 	ip = input('')
-
-	# 	if ip == '':
-	# 		ip = '127.0.0.1'
-
-	# 	res = Host.add(domain, ip)
-	# 	print(res)
-
-	# # Ver oque está na lista
-	# if acao == '2':
-	# 	print(Host.getDominios())
-
-	# # Salva os hosts que estão na Lista
-	# if acao == '3':
-	# 	res = Host.save()
-	# 	print(res)
